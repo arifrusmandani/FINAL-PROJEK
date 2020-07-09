7e971b919dcfb6345bcd9a3a5f08c5a7b85afd6f
arifrusmandani96 <arifrusmandani@gmail.com>
2020-07-09 14:41:55 +0700

    templating layouts

---
+++ resources/views/layouts/_script.blade.php
@@ -0,0 +1,17 @@
+<script src="{{asset('askme/js/jquery.min.js')}}"></script>
+<script src="{{asset('askme/js/jquery-ui-1.10.3.custom.min.js')}}"></script>
+<script src="{{asset('askme/js/jquery.easing.1.3.min.js')}}"></script>
+<script src="{{asset('askme/js/html5.js')}}"></script>
+<script src="{{asset('askme/js/twitter/jquery.tweet.js')}}"></script>
+<script src="{{asset('askme/js/jflickrfeed.min.js')}}"></script>
+<script src="{{asset('askme/js/jquery.inview.min.js')}}"></script>
+<script src="{{asset('askme/js/jquery.tipsy.js')}}"></script>
+<script src="{{asset('askme/js/tabs.js')}}"></script>
+<script src="{{asset('askme/js/jquery.flexslider.js')}}"></script>
+<script src="{{asset('askme/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('askme/js/jquery.carouFredSel-6.2.1-packed.js')}}"></script>
+<script src="{{asset('askme/js/jquery.scrollTo.js')}}"></script>
+<script src="{{asset('askme/js/jquery.nav.js')}}"></script>
+<script src="{{asset('askme/js/tags.js')}}"></script>
+<script src="{{asset('askme/js/jquery.bxslider.min.js')}}"></script>
+<script src="{{asset('askme/js/custom.js')}}"></script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+	<!-- Basic Page Needs -->
+	<meta charset="utf-8">
+	<title>Ask me – You Ask. We Answer</title>
+	<meta name="description" content="Ask me Responsive Questions and Answers Template">
+	
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Main Style -->
+	<link rel="stylesheet" href="{{asset('askme/style.css')}}">
+	
+	<!-- Skins -->
+	<link rel="stylesheet" href="{{asset('askme/css/skins/skins.css')}}">
+	
+	<!-- Responsive Style -->
+	<link rel="stylesheet" href="{{asset('askme/css/responsive.css')}}">
+	
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="{{asset('askme/images/favicon.png')}}">
+  
+</head>
+<body>
+
+@include('layouts._header')
+
+@yield('breadcrumb')
+	
+	
+	<section class="container main-content">
+		@yield('content')
+	</section><!-- End container -->
+	
+	@include('layouts._footer')
+</div><!-- End wrap -->
+
+<div class="go-up"><i class="icon-chevron-up"></i></div>
+
+<!-- js -->
+@include('layouts._script')
+<!-- End js -->
+
+</body>
+
+
+</html>
