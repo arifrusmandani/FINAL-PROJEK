309addc1dd194d1363157383cacbdcd8dd42b64a
arifrusmandani96 <arifrusmandani@gmail.com>
2020-07-12 15:50:42 +0700

    footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -28,7 +28,7 @@
 				<div class="col-md-4">
 					<div class="widget">
 						<h3 class="widget_title">Popular Questions</h3>
-						<ul class="related-posts">
+						{{-- <ul class="related-posts">
 							<li class="related-item">
 								<h3><a href="#">This is my first Question</a></h3>
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam.</p>
@@ -39,7 +39,7 @@
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam.</p>
 								<div class="clear"></div><span>Feb 22, 2014</span>
 							</li>
-						</ul>
+						</ul> --}}
 					</div>	
 				</div>
 			</div><!-- End row -->
